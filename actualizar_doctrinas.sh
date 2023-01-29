aae371209c534d51ff4bba89b73a3bd88516a22b
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2023-01-29 13:38:10 -0300

    Update actualizar_doctrinas.sh
    Agrego el personaje vax.cow

--- actualizar_doctrinas.sh
+++ actualizar_doctrinas.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
-# Programa para bajar las fortunes de adoctrinamiento peronista.
-# Requiere fortune, fortunes-es o fortune-mud
+## Programa para bajar las fortunes de adoctrinamiento peronista.
+## Funciona en Ubuntu.
+## Requiere fortune, fortunes-es o fortune-mud
 echo "Descargando todo..."
 wget -O /tmp/doctrina https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina2
 wget -O /tmp/peron.cow https://caja.texto-plano.xyz/peron/fortune-phrases/peron2.cow
 wget -O /tmp/pocho.cow https://caja.texto-plano.xyz/peron/fortune-phrases/pocho.cow
 wget -O /tmp/vacapiloto.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vacapiloto.cow
+wget -O /tmp/vax.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vax.cow
 
 echo "Moviendo ficheros..."
 mv  /tmp/peron.cow /usr/share/cowsay/cows/peron.cow
 mv  /tmp/pocho.cow /usr/share/cowsay/cows/pocho.cow
 mv  /tmp/vacapiloto.cow /usr/share/cowsay/cows
+mv  /tmp/vax.cow /usr/share/cowsay/cows
 mv /tmp/doctrina /usr/share/games/fortunes/es
 cd /usr/share/games/fortunes/es/
 
