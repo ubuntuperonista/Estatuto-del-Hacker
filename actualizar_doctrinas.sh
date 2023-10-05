d5f7f3d033083323c195565ac27d93c7792b6f93
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2023-10-05 18:21:54 -0300

    Update actualizar_doctrinas.sh

--- actualizar_doctrinas.sh
+++ actualizar_doctrinas.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
-# Programa para bajar las fortunes de adoctrinamiento peronista desde caja.texto-plano.xyz.
+# Programa para bajar las fortunes de adoctrinamiento peronista.
 # Requiere fortune, fortunes-es o fortune-mud
 echo "Descargando todo..."
 wget -O /tmp/doctrina https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina2
 wget -O /tmp/peron.cow https://caja.texto-plano.xyz/peron/fortune-phrases/peron2.cow
 wget -O /tmp/pocho.cow https://caja.texto-plano.xyz/peron/fortune-phrases/pocho.cow
 wget -O /tmp/vacapiloto.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vacapiloto.cow
+wget -O /tmp/3b2.cow https://caja.texto-plano.xyz/peron/fortune-phrases/3b2.cow
+wget -O /tmp/pdp11_84.cow https://caja.texto-plano.xyz/peron/fortune-phrases/pdp11_84.cow
 wget -O /tmp/vax.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vax.cow
 wget -O /tmp/vax3900.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vax3900.cow
 wget -O /tmp/gaucho.cow https://caja.texto-plano.xyz/peron/fortune-phrases/gaucho.cow
@@ -14,6 +16,8 @@ echo "Moviendo ficheros..."
 mv  /tmp/peron.cow /usr/share/cowsay/cows/peron.cow
 mv  /tmp/pocho.cow /usr/share/cowsay/cows/pocho.cow
 mv  /tmp/vacapiloto.cow /usr/share/cowsay/cows
+mv  /tmp/3b2.cow /usr/share/cowsay/cows
+mv  /tmp/pdp11_84.cow /usr/share/cowsay/cows
 mv  /tmp/vax.cow /usr/share/cowsay/cows
 mv  /tmp/vax3900.cow /usr/share/cowsay/cows
 mv  /tmp/gaucho.cow /usr/share/cowsay/cows
