b817ba49755f6544ea14392d63a62de9585b0668
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2023-10-05 21:50:42 -0300

    Update actualizar_doctrinas.sh

--- actualizar_doctrinas.sh
+++ actualizar_doctrinas.sh
@@ -8,6 +8,7 @@ wget -O /tmp/pocho.cow https://caja.texto-plano.xyz/peron/fortune-phrases/pocho.
 wget -O /tmp/vacapiloto.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vacapiloto.cow
 wget -O /tmp/3b2.cow https://caja.texto-plano.xyz/peron/fortune-phrases/3b2.cow
 wget -O /tmp/pdp11_84.cow https://caja.texto-plano.xyz/peron/fortune-phrases/pdp11_84.cow
+wget -O /tmp/rms.cow https://caja.texto-plano.xyz/peron/fortune-phrases/rms.cow
 wget -O /tmp/vax.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vax.cow
 wget -O /tmp/vax3900.cow https://caja.texto-plano.xyz/peron/fortune-phrases/vax3900.cow
 wget -O /tmp/gaucho.cow https://caja.texto-plano.xyz/peron/fortune-phrases/gaucho.cow
@@ -18,6 +19,7 @@ mv  /tmp/pocho.cow /usr/share/cowsay/cows/pocho.cow
 mv  /tmp/vacapiloto.cow /usr/share/cowsay/cows
 mv  /tmp/3b2.cow /usr/share/cowsay/cows
 mv  /tmp/pdp11_84.cow /usr/share/cowsay/cows
+mv  /tmp/rms.cow /usr/share/cowsay/cows
 mv  /tmp/vax.cow /usr/share/cowsay/cows
 mv  /tmp/vax3900.cow /usr/share/cowsay/cows
 mv  /tmp/gaucho.cow /usr/share/cowsay/cows
