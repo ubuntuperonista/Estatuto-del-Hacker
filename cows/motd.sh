e77aa238a76a4588c5e8d92b1d6cbe6bcfa96aa3
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2024-01-08 15:35:56 -0300

    Create motd.sh

---
+++ cows/motd.sh
@@ -0,0 +1,11 @@
+# Lanza un fortune al azar dicho por un personaje de cowsay al azar
+function cowsayfortune {
+    NUMOFCOWS=`cowsay -l | tail -n +2 | wc -w`
+    WHICHCOW=$((RANDOM%$NUMOFCOWS+1))
+    THISCOW=`cowsay -l | tail -n +2 | sed -e 's/\ /\'$'\n/g' | sed $WHICHCOW'q;d'`
+
+    #echo "Selected cow: ${THISCOW}, from ${WHICHCOW}"
+    fortune | cowsay -f $THISCOW -W 100
+}
+
+cowsayfortune
