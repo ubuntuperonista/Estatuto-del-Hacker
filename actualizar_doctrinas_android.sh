d2f0202327dd27b61005adf63cc4863f470dd53b
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2024-05-28 13:36:28 -0300

    Update actualizar_doctrinas_android.sh

--- actualizar_doctrinas_android.sh
+++ actualizar_doctrinas_android.sh
@@ -2,27 +2,27 @@
 # Programa para bajar frases de adoctrinamiento en Android
 # Requiere termux, bash, fortune, fortunes-es o fortune-mud
 echo "Descargando Fortunes para Android..."
-cd ~/Downloads ;
-wget -O ~/Downloads/doctrina https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina2
-wget -O ~/Downloads/rms https://caja.texto-plano.xyz/peron/fortune-phrases/rms2
+cd /data/data/com.termux/files/usr/tmp/ ;
+wget https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina2
+wget https://caja.texto-plano.xyz/peron/fortune-phrases/rms2
 
 echo "Descargando cowfiles para GNU..."
-wget -O /Downloads/cows.tar.gz https://caja.texto-plano.xyz/peron/fortune-phrases/cows/cows.tar.gz
-tar xzf /Downloads/cows.tar.gz
+wget https://caja.texto-plano.xyz/peron/fortune-phrases/cows/cows.tar.gz
+tar xzf /data/data/com.termux/files/usr/tmp/cows.tar.gz
 
 echo "Moviendo ficheros .cow..."
-mv -f /Downloads/cows/*.cow /data/data/com.termux/files/share/cowsay/cows/
-rm -r /Downloads/cows/
+mv -f /data/data/com.termux/files/usr/tmp/cows/*.cow /data/data/com.termux/files/share/cowsay/cows/
+rm -r /data/data/com.termux/files/usr/tmp/cows/
 
 echo "Moviendo fortunes..."
-mv -f /Downloads/doctrina2 /data/data/com.termux/files/usr/share/games/fortunes/doctrina
-mv -f /Downloads/rms2 /data/data/com.termux/files/usr/share/games/fortunes/rms
+mv -f /data/data/com.termux/files/usr/tmp/doctrina2 /data/data/com.termux/files/usr/share/games/fortunes/doctrina
+mv -f /data/data/com.termux/files/usr/tmp/rms2 /data/data/com.termux/files/usr/share/games/fortunes/rms
 
 echo "Creando ejecutables..."
-echo "fortune -f doctrina" > /data/data/com.termux/files/usr/local/bin/doctrina
-echo "fortune -f rms" > /data/data/com.termux/files/usr/local/bin/rms
-chmod +x /data/data/com.termux/files/usr/local/bin/doctrina
-chmod +x /data/data/com.termux/files/usr/local/bin/rms
+echo "fortune doctrina" > /data/data/com.termux/files/usr/bin/doctrina
+echo "fortune rms" > /data/data/com.termux/files/usr/bin/rms
+chmod +x /data/data/com.termux/files/usr/bin/doctrina
+chmod +x /data/data/com.termux/files/usr/bin/rms
 
 #Dispara una fortune adoctrinante de demostración
 fortune -s doctrina | /usr/games/cowsay -f 17oct
