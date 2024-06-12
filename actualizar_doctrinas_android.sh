15c565c1d43d7592c6f86653c54713c2fb906303
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2024-06-11 21:28:18 -0300

    Update actualizar_doctrinas_android.sh

--- actualizar_doctrinas_android.sh
+++ actualizar_doctrinas_android.sh
@@ -1,28 +1,28 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# Programa para bajar frases de adoctrinamiento en Android
+# Programa para bajar frases de adoctrinamiento en Termux para Android
 # Requiere termux, bash, fortune, fortunes-es o fortune-mud
 echo "Descargando Fortunes para Android..."
-cd /data/data/com.termux/files/usr/tmp/ ;
+cd $PREFIX/tmp/ ;
 wget https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina2
 wget https://caja.texto-plano.xyz/peron/fortune-phrases/rms2
 
-echo "Descargando cowfiles para GNU..."
+echo "Descargando cowfiles para Android..."
 wget https://caja.texto-plano.xyz/peron/fortune-phrases/cows/cows.tar.gz
-tar xzf /data/data/com.termux/files/usr/tmp/cows.tar.gz
+tar xzf $PREFIX/tmp/cows.tar.gz
 
 echo "Moviendo ficheros .cow..."
-mv -f /data/data/com.termux/files/usr/tmp/cows/*.cow /data/data/com.termux/files/share/cowsay/cows/
+mv $PREFIX/tmp/cows/*.cow /data/data/com.termux/files/share/cowsay/cows/
 rm -r /data/data/com.termux/files/usr/tmp/cows/
 
 echo "Moviendo fortunes..."
-mv -f /data/data/com.termux/files/usr/tmp/doctrina2 /data/data/com.termux/files/usr/share/games/fortunes/doctrina
-mv -f /data/data/com.termux/files/usr/tmp/rms2 /data/data/com.termux/files/usr/share/games/fortunes/rms
+mv $PREFIX/tmp/doctrina2 $PREFIX/share/games/fortunes/doctrina
+mv $PREFIX/tmp/rms2 $PREFIX/share/games/fortunes/rms
 
 echo "Creando ejecutables..."
-echo "fortune /data/data/com.termux/files/usr/share/games/fortunes/doctrina" > /data/data/com.termux/files/usr/bin/doctrina
-echo "fortune /data/data/com.termux/files/usr/share/games/fortunes/rms" > /data/data/com.termux/files/usr/bin/rms
-chmod +x /data/data/com.termux/files/usr/bin/doctrina
-chmod +x /data/data/com.termux/files/usr/bin/rms
+echo "fortune $PREFIX/share/games/fortunes/doctrina" > $PREFIX/bin/doctrina
+echo "fortune $PREFIX/share/games/fortunes/rms" > $PREFIX/bin/rms
+chmod +x $PREFIX/bin/doctrina
+chmod +x $PREFIX/bin/rms
 
 #Dispara una fortune adoctrinante de demostración
-fortune -s /data/data/com.termux/files/usr/share/games/fortunes/doctrina | cowsay -f 17oct
+fortune -s $PREFIX/share/games/fortunes/doctrina | cowsay -f 17oct
