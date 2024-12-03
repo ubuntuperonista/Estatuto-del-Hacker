27d39babd4038ee6498b456fb9840cbfb9598384
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2024-12-03 15:23:49 -0300

    Create vaca.sh

---
+++ vaca.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# vaca
+# Un cowfile al azar que dice fortune de adoctrinamiento cada vez que se ejecuta
+function cowsayfortune {
+    CANTCOWFILES=`cowsay -l | tail -n +2 | wc -w`
+    CUALCOWFILE=$((RANDOM%$CANTCOWFILES+1))
+    ESTECOWFILE=`cowsay -l | tail -n +2 | sed -e 's/\ /\'$'\n/g' | sed $CUALCOWFILE'q;d'`
+
+    #echo "Cowfile elegida: ${ESTACOW}, desde ${CUALCOW}"
+    fortune doctrina | cowsay -f $ESTECOWFILE -W 60
+}
+
+cowsayfortune
