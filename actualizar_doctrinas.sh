dfc0291a1378f5e769f459f81f0b57bd098f12c5
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2024-12-03 15:35:02 -0300

    Update actualizar_doctrinas.sh

--- actualizar_doctrinas.sh
+++ actualizar_doctrinas.sh
@@ -4,18 +4,16 @@
 echo "Descargando Fortunes para GNU..."
 cd /tmp ;
 wget -O /tmp/doctrina https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina2
-wget -O /tmp/doctrina.sh https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina.sh
 wget -O /tmp/rms https://caja.texto-plano.xyz/peron/fortune-phrases/rms2
-wget -O /tmp/rms.sh https://caja.texto-plano.xyz/peron/fortune-phrases/rms.sh
 wget -O /tmp/excusas https://caja.texto-plano.xyz/peron/fortune-phrases/excusas2
 wget -O /tmp/pintadas https://caja.texto-plano.xyz/peron/fortune-phrases/pintadas
+echo "Descargando programas para GNU..."
+wget -O /tmp/doctrina.sh https://caja.texto-plano.xyz/peron/fortune-phrases/doctrina.sh
+wget -O /tmp/vaca.sh https://caja.texto-plano.xyz/peron/fortune-phrases/vaca.sh
+wget -O /tmp/rms.sh https://caja.texto-plano.xyz/peron/fortune-phrases/rms.sh
 echo "Descargando cowfiles para GNU..."
 wget -O /tmp/cows.tar.gz https://caja.texto-plano.xyz/peron/fortune-phrases/cows/cows.tar.gz
 tar xzf /tmp/cows.tar.gz
-
-echo "Moviendo ficheros .cow..."
-mv -f /tmp/cows/*.cow /usr/share/cowsay/cows/
-rm -r /tmp/cows/
 
 echo "Moviendo fortunes..."
 mv -f /tmp/doctrina /usr/share/games/fortunes/es
@@ -23,11 +21,17 @@ mv -f /tmp/rms /usr/share/games/fortunes/es
 mv -f /tmp/excusas /usr/share/games/fortunes/es
 mv -f /tmp/pintadas /usr/share/games/fortunes/es
 
+echo "Moviendo cowfiles .cow..."
+mv -f /tmp/cows/*.cow /usr/share/cowsay/cows/
+rm -r /tmp/cows/
+
 echo "Moviendo programas..."
 mv -f /tmp/doctrina.sh /usr/local/bin/doctrina
 mv -f /tmp/rms.sh /usr/local/bin/rms
+mv -f /tmp/vaca.sh /usr/local/bin/vaca
 chmod +x /usr/local/bin/doctrina
 chmod +x /usr/local/bin/rms
+chmod +x /usr/local/bin/vaca
 
 echo "OK!"
 echo 'Actualizando Doctrinas...'
