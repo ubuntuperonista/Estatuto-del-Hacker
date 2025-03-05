0bc666074e4a20d920b840dab2be5c9b289f63b0
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2025-03-05 16:01:06 -0300

    Create vaca_android.sh

---
+++ vaca_android.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# vaca_android.sh
+# Un cowfile al azar que dice fortune de adoctrinamiento cada vez que se ejecuta
+function cowsayfortune {
+    CANTCOWFILES=`cowsay -l | tail -n +2 | wc -w`
+    CUALCOWFILE=$((RANDOM%$CANTCOWFILES+1))
+    ESTECOWFILE=`cowsay -l | tail -n +2 | sed -e 's/\ /\'$'\n/g' | sed $CUALCOWFILE'q;d'`
+
+    #echo "Cowfile elegida: ${ESTACOW}, desde ${CUALCOW}"
+    fortune -s $PREFIX/share/games/fortunes/doctrina | cowsay -f $ESTECOWFILE
+}
+
+cowsayfortune
